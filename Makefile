81867f7f45dd6c23dfdd2a5e24eec8512d3a0fba
Erik Bryant <erikbryantology@gmail.com>
2023-12-03 20:40:45 -0500

    Enable profiling

--- Makefile
+++ Makefile
@@ -11,5 +11,10 @@ run: test
 	go build
 	./gophernet
 
+profile: test
+	go build
+	./gophernet -cpuprofile cpu.prof
+	echo top | go tool pprof cpu.prof
+
 # Targets that do not represent actual files
-.PHONY: fmt test vet run
+.PHONY: fmt test vet run profile
--- main.go
+++ main.go
@@ -1,26 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
+	"runtime/pprof"
+)
+
+var (
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 )
 
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	config, inputs, labels, testInputs, testLabels := iris()
 
 	// The neural network is not scalable enough to handing the
 	// handwriting images. The network would need at least one
 	// more hidden layer and would need hours (days?) of training
 	// time to generate a reliable model.
-	// config, inputs, labels, testInputs, testLabels = handwriting()
+	config, inputs, labels, testInputs, testLabels = handwriting()
 
 	network := newNetwork(config)
 	if err := network.train(inputs, labels); err != nil {
 		log.Fatal(err)
 	}
 
-	// Evaluate the neural network
-	accuracy, err := network.evaluate(testInputs, testLabels)
+	// Evaluate the neural network's prediction accuracy
+	accuracy, err := network.accuracy(testInputs, testLabels)
 	if err != nil {
 		log.Fatal(err)
 	}
