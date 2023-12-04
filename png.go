2d9a35ec9fbeb27d298eb2e54972e83338ab4891
Erik Bryant <erikbryantology@gmail.com>
2023-12-03 20:41:21 -0500

    Find the smallest common bounding box

--- png.go
+++ png.go
@@ -7,6 +7,13 @@ import (
 	"os"
 )
 
+var (
+	loosestBounds = image.Rectangle{
+		Min: image.Point{Y: 99999, X: 99999},
+		Max: image.Point{Y: 0, X: 0},
+	}
+)
+
 // readPNG reads a PNG file and returns it as a grayscale image
 func readPNG(filename string) (*image.Gray, error) {
 	// Open the PNG file
@@ -29,15 +36,86 @@ func readPNG(filename string) (*image.Gray, error) {
 	return grayscale, nil
 }
 
+func miny(img *image.Gray) int {
+	bounds := img.Bounds()
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			if img.GrayAt(y, x).Y != 255 {
+				return y
+			}
+		}
+	}
+
+	return bounds.Max.Y
+}
+
+func minx(img *image.Gray) int {
+	bounds := img.Bounds()
+
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			if img.GrayAt(y, x).Y != 255 {
+				return x
+			}
+		}
+	}
+
+	return bounds.Max.X
+}
+
+func maxy(img *image.Gray) int {
+	bounds := img.Bounds()
+
+	for y := bounds.Max.Y - 1; y >= bounds.Min.Y; y-- {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			if img.GrayAt(y, x).Y != 255 {
+				return y
+			}
+		}
+	}
+
+	return bounds.Min.Y
+}
+
+func maxx(img *image.Gray) int {
+	bounds := img.Bounds()
+
+	for x := bounds.Max.X - 1; x >= bounds.Min.X; x-- {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			if img.GrayAt(y, x).Y != 255 {
+				return y
+			}
+		}
+	}
+
+	return bounds.Min.X
+}
+
+// boundingBox returns the bounding dimensions of the figure
+func boundingBox(img *image.Gray) image.Rectangle {
+	bbox := image.Rectangle{}
+
+	bbox.Min.Y = miny(img)
+	bbox.Max.Y = maxy(img)
+	bbox.Min.X = minx(img)
+	bbox.Max.X = maxx(img)
+
+	return bbox
+}
+
 // imageToSlice converts a grayscale PNG image to a slice of floats
 func imageToSlice(img *image.Gray) []float64 {
-	bounds := img.Bounds()
 	s := []float64{}
 
-	// Skip the framing pixels; they are almost always just white
-	frameWidth := 10
-	for y := bounds.Min.Y + frameWidth; y < bounds.Max.Y-frameWidth; y++ {
-		for x := bounds.Min.X + frameWidth; x < bounds.Max.X-frameWidth; x++ {
+	// Use the uniform minimal bounding box
+	minbbox := image.Rectangle{
+		Min: image.Point{Y: 5, X: 22},
+		Max: image.Point{Y: 114, X: 72},
+	}
+
+	for y := minbbox.Min.Y; y < minbbox.Max.Y; y++ {
+		for x := minbbox.Min.X; x < minbbox.Max.X; x++ {
 			c := img.GrayAt(y, x)
 			scalar := 0.0
 			if c.Y == 0 {
@@ -55,5 +133,13 @@ func slicePNG(filename string) ([]float64, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// bbox := boundingBox(img)
+	// loosestBounds.Min.Y = min(bbox.Min.Y, loosestBounds.Min.Y)
+	// loosestBounds.Min.X = min(bbox.Min.X, loosestBounds.Min.X)
+	// loosestBounds.Max.Y = max(bbox.Max.Y, loosestBounds.Max.Y)
+	// loosestBounds.Max.X = max(bbox.Max.X, loosestBounds.Max.X)
+	// fmt.Println(loosestBounds)
+
 	return imageToSlice(img), nil
 }
